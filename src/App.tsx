5765f1fd63208b5ee18b7010477c1e1d2e054121
JSiapo <jsiapo@unitru.edu.pe>
2021-01-09 15:25:06 -0500

    Create Notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import CardImage from './components/widgets/default/CardImage'
+import CardImage from './components/widgets/default/CardImage';
+import NavBar from './components/widgets/default/NavBar';
 function App() {
   return (
-<div className="container mx-auto my-auto py-4">
+    <>
+      <NavBar />
+      <div className='container mx-auto my-auto py-4'>
+        <CardImage
+          image='https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80'
+          title='Case study'
+          subtitle='Finding customers for your new business!!!'
+          description='Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.'
+        />
 
-
-<CardImage 
-image='https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80' 
-title='Case study' 
-subtitle='Finding customers for your new business!!!'
-description='Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.'
-/>
-
-<button type='button' className='bg-blue-500 md:bg-green-400 hover:bg-red-700 md:hover:bg-blue-700 text-white rounded-xl font-bold py-2 px-4' >My Button</button>
-
-
-</div>
-
+        <button
+          type='button'
+          className='bg-blue-500 md:bg-green-400 hover:bg-red-700 md:hover:bg-blue-700 text-white rounded-xl font-bold py-2 px-4'
+        >
+          My Button
+        </button>
+      </div>
+    </>
   );
 }
 
