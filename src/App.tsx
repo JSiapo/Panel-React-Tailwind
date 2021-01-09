e73d4e2ab9ccb1b68789fee39a479445d98c457b
JSiapo <jsiapo@unitru.edu.pe>
2021-01-09 13:46:52 -0500

    PWA

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 <CardImage 
 image='https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80' 
 title='Case study' 
-subtitle='Finding customers for your new business'
+subtitle='Finding customers for your new business!!!'
 description='Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.'
 />
 
